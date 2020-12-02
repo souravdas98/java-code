23d23486ccd1640586f075545b9903409c45e415
Sourav Das <54032125+souravdas98@users.noreply.github.com>
2020-12-02 22:10:09 +0530

    Even and Odd number count in 2D Array

---
+++ Array_2d.java
@@ -0,0 +1,55 @@
+import java.util.*;
+class Main{
+    void Take_Input(){
+       Scanner sc=new Scanner(System.in);
+       System.out.println("enter the rows:"); 
+       int rows=sc.nextInt();
+       System.out.println("enter the columns:"); 
+       int columns=sc.nextInt();
+       
+       System.out.println("Enter array elements : ");    
+        
+       int a[][]=new int[rows][columns];
+        
+          
+        for(int i=0; i<a.length;i++)
+         {            
+            for(int j=0; j<a[0].length;j++)
+            {
+                a[i][j]=sc.nextInt();
+            }
+         }
+         Count_Odd_Even(a);
+
+    }
+    void Count_Odd_Even(int a[][]){
+        int e=0;
+         int o=0;
+         for(int i = 0; i <a.length; i++){    
+            for(int j = 0; j <a[0].length; j++){    
+              if(a[i][j] % 2 == 0) 
+              {
+                e++; 
+                System.out.println("Even numbers: " + a[i][j]); 
+              }
+
+              else   
+              { 
+                o++;
+                System.out.println("Odd numbers: " + a[i][j]); 
+              }   
+            }    
+        }    
+            
+        System.out.println("Frequency of odd numbers: " + o);    
+        System.out.println("Frequency of even numbers: " + e);    
+    }        
+    
+
+}
+class Array_2d{
+    public static void main(String[]args){
+        Main a=new Main();
+        a.Take_Input();
+    }
+}
