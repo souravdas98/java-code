c4a0198decdb893b2ce5077732bc62dffad229c2
Sourav Das <54032125+souravdas98@users.noreply.github.com>
2020-12-03 12:52:54 +0530

    search elements and number of occurrences
    Create one 1D array in java.search for some elements and number of occurrences inputted by the user

--- Array_1d.java
+++ Array_1d.java
@@ -4,50 +4,48 @@ import javax.lang.model.util.ElementScanner6;
 
 import java.lang.*;
 class Main{
+    int a[];
     void Take_Input(){
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the length of the array:");
         int n=sc.nextInt();
-        int a[]=new int[n];
+        a=new int[n];
         for(int i=0;i<a.length;i++){
             System.out.println("enter the "+(i+1)+" elemrnt of the array:");
             a[i]=sc.nextInt();
         }
-        System.out.println("array element are:");
-        for(int i=0;i<a.length;i++){
-            System.out.println(a[i]);
-        }
         System.out.println("----------------------------------------------------");
         
-        Count_Odd_Even(a);
+        
 
     }
-    void Count_Odd_Even(int a[]){
-        int o=0;
-        int e=0;
+    void Search_Occurence(){
+        int c=0;
+        System.out.println("Enter the element to be serach:");
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
         for(int i=0;i<a.length;i++){
-            if(a[i]%2==0)
-            {
-            System.out.println("even element are :"+a[i]);
-            e++;
-            }
-            else{
-                System.out.println("odd element are :"+a[i]);
-                o++;
+            if(x==a[i]){
+                c++;
             }
 
-            System.out.println("----------------------------------------------------");
 
         }
-        System.out.println("odd number :"+o);
-        System.out.println("even number :"+e);
-        System.out.println("----------------------------------------------------");
+        if(c==0){
+            System.out.println(x+" element is not found");
+        }
+        else{
+        System.out.println(x+" element is present in an array the Occurrences of the number is "+c);
+        }
+       
 
     }
+    
 }
 class Array_1d{
     public static void main(String[]args){
         Main a=new Main();
         a.Take_Input();
+        a.Search_Occurence();
     }
 }
